97cb9d9446688353afa60cf5f1c4ac2057c60519
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-23 12:10:10 +0900

    Fix sql

--- containers/database/init.sql
+++ containers/database/init.sql
@@ -1,32 +1,32 @@
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  name varchar(100),
-  email varchar(100),
-  password varchar(200),
-  icon_url varchar(10000)
+  name varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(200) NOT NULL CONSTRAINT valid_password_length CHECK(LENGTH(password) > 4),
+  icon_url varchar(10000) NOT NULL
 );
 
 CREATE TABLE posts(
   id SERIAL PRIMARY KEY,
-  user_id integer REFERENCES users (id),
-  title varchar(100),
-  body varchar(8000),
-  image_id integer
+  user_id integer REFERENCES users (id) ON DELETE CASCADE,
+  title varchar(100) NOT NULL,
+  body varchar(8000) NOT NULL,
+  thumbnail_id integer NOT NULL
 );
 
 CREATE TABLE images(
   id SERIAL PRIMARY KEY,
-  post_id integer REFERENCES posts (id),
-  url varchar(10000) 
-);
-
-CREATE TABLE post_tags(
-  post_id integer REFERENCES posts (id),
-  tag_id integer REFERENCES tags (id),
-  PRIMARY KEY(post_id, tag_id)
+  post_id integer REFERENCES posts (id) ON DELETE CASCADE,
+  url varchar(10000) NOT NULL 
 );
 
 CREATE TABLE tags(
   id SERIAL PRIMARY KEY,
-  name varchar(100)
+  name varchar(100) NOT NULL
+);
+
+CREATE TABLE post_tags(
+  post_id integer REFERENCES posts (id) ON DELETE CASCADE,
+  tag_id integer REFERENCES tags (id),
+  PRIMARY KEY(post_id, tag_id)
 );
