49a53c97aba4382ce721037c178cf49080e520a7
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-24 12:52:33 +0900

    Add router

--- src/public/index.php
+++ src/public/index.php
@@ -1,20 +1,27 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>kenshu-backend-php</title>
-    <script defer src="https://cdn.tailwindcss.com"></script>
-  </head>
-  <body>
-    <h1 class="text-3xl font-bold underline">Kenshu backend php</h1>
-    <ul>
-      <li></li>
-    </ul>
-    <form action="create_post.php" method="post">
-      タイトル: <input type="text" name="title" />
-      本文: <input type="text" name="body" />
-      <input type="submit" />
-    </form>
-  </body>
-</html>
+<?php
+  require_once(dirname(__DIR__, 1)."/class/PgConnect.php");
+  require_once(dirname(__DIR__, 1)."/class/PostClient.php");
+  require_once(dirname(__DIR__, 1)."/class/PostPayload.php");
+
+  $request_url = $_SERVER["REQUEST_URI"];
+  $request_method = $_SERVER["REQUEST_METHOD"];
+
+  if ($request_method === "GET" && $request_url === "/") {
+    $html_file_path = dirname(__DIR__).'/page/index.html';
+
+    $html = file_get_contents($html_file_path);
+    echo $html;
+
+  } else if ($request_method === "POST" && $request_url === "/post")  {
+    $title = htmlspecialchars($_POST['title']);
+    $body = htmlspecialchars($_POST['body']);
+
+    $pg_client = PgConnect::getClient();
+    $post_clinet = new PostClient(pdo: $pg_client);
+    $payload = new PostPayload(2, $title, $body, 1);
+    $post_clinet->createPost($payload);
+
+
+  } else if ($request_method === "GET" && $request_url === "/huga"){
+    echo "huga page";
+  }
