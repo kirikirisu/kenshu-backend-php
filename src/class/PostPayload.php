c15ae1a819ea249e82903674e602eabaf0801652
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-29 15:32:02 +0900

    Constructor proparty promotion

--- src/class/PostPayload.php
+++ src/class/PostPayload.php
@@ -1,14 +1,9 @@
 <?php
   class PostPayload {
-    public int $user_id;
-    public string $title; 
-    public string $body;
-    public int $thumbnail_id;
-
-    public function __construct(int $user_id, string $title, string $body, int $thumbnail_id) {
-      $this->user_id = $user_id;
-      $this->title = $title;
-      $this->body = $body;
-      $this->thumbnail_id = $thumbnail_id;
+    public function __construct(
+      public int $user_id, 
+      public string $title, 
+      public string $body, 
+      public int $thumbnail_id) {
     }
   }
