c5bafb4aafb7c618877476ab04a44c30fef07a55
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-29 12:34:33 +0900

    Add horizontal card for post list view

--- src/view/PageComposer.php
+++ src/view/PageComposer.php
@@ -10,29 +10,25 @@
     * @param Post[] $data_chunk
     * @param InputError[] $error_list
     */
-<<<<<<< HEAD:src/view/PageComposer.php
-    public function topPage($data_chunk) {
-      $html_file_path = dirname(__DIR__).'/view/html/top.html';
-=======
     public function topPage($data_chunk, $error_list = null) {
-      $html_file_path = dirname(__DIR__).'/page/top.html';
->>>>>>> 968dfe0 (Validate form input):src/class/PageComposer.php
-      $top_page_html = file_get_contents($html_file_path);
+      $top_page_base_html = file_get_contents(dirname(__DIR__).'/ui/page/top.html');
+      $horizontal_card = file_get_contents(dirname(__DIR__).'/ui/part/horizontal-card.html');
+
       $post_list_fragment = "";
 
       foreach($data_chunk as $post) {
-        $post_list_fragment = $post_list_fragment."<li>".$post->title."</li>\n";
+        $post_list_fragment = $post_list_fragment.str_replace("%title%", $post->title, $horizontal_card);
       }
 
-      $this->page = str_replace("%post_list%", $post_list_fragment, $top_page_html);
+      $this->page = str_replace("%post_list%", $post_list_fragment, $top_page_base_html);
 
       if ($error_list) {
         foreach($error_list as $error) {
           if ($error->field === "title") {
-            $this->page = str_replace("%invalid_title%", "<p>".$error->message."</p>", $this->page);
+            $this->page = str_replace("%invalid_title%", '<p class="mt-1 text-pink-600">'.$error->message."</p>", $this->page);
           }
           if ($error->field === "body") {
-            $this->page = str_replace("%invalid_body%", "<p>".$error->message."</p>", $this->page);
+            $this->page = str_replace("%invalid_body%", '<p class="mt-1 text-pink-600">'.$error->message."</p>", $this->page);
           }
         }
       }
