69640d3411fe5872022ecd15e8bd7d9596687a13
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-29 15:34:12 +0900

    Make endpoints plural nouns

--- src/public/index.php
+++ src/public/index.php
@@ -7,8 +7,8 @@
   if ($request_method === "GET" && $request_url === "/") {
     PostHandler::getPostListPage();
     
-  } else if ($request_method === "POST" && $request_url === "/post")  {
+  } else if ($request_method === "POST" && $request_url === "/posts")  {
     PostHandler::createPost();
 
-  } else if ($request_method === "GET" && $request_url === "/post/:id"){
+  } else if ($request_method === "GET" && $request_url === "/posts/:id"){
   }
