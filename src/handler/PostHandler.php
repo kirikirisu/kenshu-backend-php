3affbc1cc202bc29613c07493abf12aad0f468d7
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-29 10:59:28 +0900

    Validate form input

--- src/handler/PostHandler.php
+++ src/handler/PostHandler.php
@@ -2,6 +2,7 @@
   require_once(dirname(__DIR__, 1)."/client/PostClient.php");
   require_once(dirname(__DIR__, 1)."/client/PostPayload.php");
   require_once(dirname(__DIR__, 1)."/view/PageComposer.php");
+  require_once(dirname(__DIR__, 1)."/view/InputError.php");
 
   class PostHandler 
   {
@@ -19,6 +20,20 @@
       $body = htmlspecialchars($_POST['post-body']);
       
       $post_client = new PostClient();
+
+      $raw_post = new ValidatePost(title: $title, body: $body);
+      $error_list = $raw_post->validate();
+
+      $post_client = new PostClient(pdo: PgConnect::getClient());
+      if (count($error_list) > 0) {
+        $post_list = $post_client->getPostList();
+
+        $compose = new PageComposer();
+        $compose->topPage($post_list, $error_list);
+        $compose->renderHTML();
+        exit;
+      }
+      
       $payload = new PostPayload(2, $title, $body, 1);
       $post_client->createPost($payload);
 
@@ -26,4 +41,31 @@
       exit;
     }
 
+
   }
+
+class ValidatePost {
+  public string $title;
+  public string $body;
+  /** @var InputError[] $error_list */
+  public $error_list = [];
+
+  public function __construct(string $title, string $body)
+  {
+    $this->title = $title;
+    $this->body = $body;
+  }
+
+  /** @return InputError[] */
+  public function validate() {
+    if ($this->title === "") {
+      $this->error_list[] = new InputError("タイトルを入力してください。", "title"); 
+    }
+    if ($this->body === "") {
+      $this->error_list[] = new InputError("本文を入力してください。", "body");
+    }
+
+    return $this->error_list;
+  }
+
+}
--- src/view/PageComposer.php
+++ src/view/PageComposer.php
@@ -1,5 +1,6 @@
 <?php
   require_once(dirname(__DIR__, 1)."/model/Post.php");
+  require_once(dirname(__DIR__, 1)."/class/InputError.php");
 
   class PageComposer
   {
@@ -7,9 +8,15 @@
 
     /**
     * @param Post[] $data_chunk
+    * @param InputError[] $error_list
     */
+<<<<<<< HEAD:src/view/PageComposer.php
     public function topPage($data_chunk) {
       $html_file_path = dirname(__DIR__).'/view/html/top.html';
+=======
+    public function topPage($data_chunk, $error_list = null) {
+      $html_file_path = dirname(__DIR__).'/page/top.html';
+>>>>>>> 968dfe0 (Validate form input):src/class/PageComposer.php
       $top_page_html = file_get_contents($html_file_path);
       $post_list_fragment = "";
 
@@ -18,6 +25,20 @@
       }
 
       $this->page = str_replace("%post_list%", $post_list_fragment, $top_page_html);
+
+      if ($error_list) {
+        foreach($error_list as $error) {
+          if ($error->field === "title") {
+            $this->page = str_replace("%invalid_title%", "<p>".$error->message."</p>", $this->page);
+          }
+          if ($error->field === "body") {
+            $this->page = str_replace("%invalid_body%", "<p>".$error->message."</p>", $this->page);
+          }
+        }
+      }
+
+      $this->page = str_replace("%invalid_title%", "", $this->page);
+      $this->page = str_replace("%invalid_body%", "", $this->page);
     }
 
     public function postDetailPage() {}
