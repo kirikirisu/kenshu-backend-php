36fb9fd161a105291760cea5d60493f7f0e5130d
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-06-07 15:45:00 +0900

    Inject PostClient

--- src/handler/PostHandler.php
+++ src/handler/PostHandler.php
@@ -7,15 +7,14 @@ require_once (dirname(__DIR__, 1)) . "/lib/Errors/InputError.php";
 
 class PostHandler
 {
-    public static function getPostListPage(PageComposer $compose): void
+    public static function getPostListPage(PageComposer $compose, PostClient $post_client): void
     {
-        $post_client = new PostClient();
         $post_list = $post_client->getPostList();
 
         $compose->topPage($post_list)->renderHTML();
     }
 
-    public static function createPost(PageComposer $compose): void
+    public static function createPost(PageComposer $compose, PostClient $post_client): void
     {
         $title = $_POST['post-title'];
         $body = $_POST['post-body'];
@@ -23,30 +22,27 @@ class PostHandler
         $error_list = static::validatePost(title: $title, body: $body);
         if (count($error_list) > 0) static::renderTopPageWithError($compose, $error_list);
 
-        $post_client = new PostClient();
         $payload = new IndexPostDto(2, $title, $body, 1);
         $post_client->createPost($payload);
 
         header("Location: http://localhost:8080", true, 303);
     }
 
-    public static function getPostDetailPage(int $post_id, PageComposer $compose)
+    public static function getPostDetailPage(int $post_id, PageComposer $compose, PostClient $post_client)
     {
-        $post_client = new PostClient();
         $post = $post_client->getPostById($post_id);
 
         $compose->postDetailPage($post)->renderHTML();
     }
 
-    public static function getEditPage(int $post_id, PageComposer $compose)
+    public static function getEditPage(int $post_id, PageComposer $compose, PostClient $post_client)
     {
-        $post_client = new PostClient();
         $post = $post_client->getPostById($post_id);
 
         $compose->getPostEditPage(post: $post)->renderHTML();
     }
 
-    public static function updatePost(int $post_id, PageComposer $compose): void
+    public static function updatePost(int $post_id, PostClient $post_client): void
     {
         $body = file_get_contents('php://input');
         $data = json_decode($body);
@@ -58,7 +54,6 @@ class PostHandler
             exit;
         }
 
-        $post_client = new PostClient();
         $dto = new UpdatePostDto(title: $data->title, body: $data->body, thumbnail_id: 1);
         $post_client->updatePost($post_id, $dto);
 
@@ -67,9 +62,8 @@ class PostHandler
         echo json_encode(array('message' => 'Patch request succeeded', 'redirectUrl' => $redirect_url));
     }
 
-    public static function deletePost(string $post_id): void
+    public static function deletePost(string $post_id, PostClient $post_client): void
     {
-        $post_client = new PostClient();
         $post_client->deletePost($post_id);
 
 //        header("Location:  http://localhost:8080", true,303);
