e933e3a8df1016a0c3e6101c72893724c7311042
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-06-08 13:00:46 +0900

    insert image

--- src/handler/PostHandler.php
+++ src/handler/PostHandler.php
@@ -4,6 +4,7 @@ require_once(dirname(__DIR__, 1) . "/model/dto/IndexPostDto.php");
 require_once(dirname(__DIR__, 1) . "/model/dto/UpdatePostDto.php");
 require_once(dirname(__DIR__, 1) . "/lib/PageComposer.php");
 require_once (dirname(__DIR__, 1)) . "/lib/Errors/InputError.php";
+require_once(dirname(__DIR__, 1) . "/client/ImageClient.php");
 
 class PostHandler
 {
@@ -22,31 +23,37 @@ class PostHandler
         $image_list = $_FILES['images'];
         $main_image = $_POST['main-image'];
 
-        $error_list = static::validatePost(title: $title, body: $body, main_image: $main_image);
-        if (count($error_list) > 0) static::renderTopPageWithError($compose, $error_list);
+//        $error_list = static::validatePost(title: $title, body: $body, main_image: $main_image);
+//        if (count($error_list) > 0) static::renderTopPageWithError($compose, $error_list);
 
-        $thumbnail_image_id = "";
-        $upload_dir = dirname(__DIR__) . "/public/assets/images/";
+        $thumbnail_image_uri = "";
+        $image_uri_list = [];
+        $img_public_dir = "/assets/images/";
         foreach ($image_list['error'] as $key => $error) {
             if ($error == UPLOAD_ERR_OK) {
                 $file_name = $_FILES['images']['name'][$key];
-                $temp_file_path = $_FILES['images']['tmp_name'][$key];
 
                 $uniqu_file_name = sprintf('%s_%s.%s', pathinfo($file_name, PATHINFO_FILENAME), time(), pathinfo($file_name, PATHINFO_EXTENSION));
-                $target_file_path = sprintf('%s%s', $upload_dir, $uniqu_file_name );
+                $image_uri = sprintf('%s%s', $img_public_dir, $uniqu_file_name);
+                $image_uri_list[] = $image_uri;
 
-                if ($file_name === $main_image) {
-                    $thumbnail_image_id = $target_file_path;
-                }
-                move_uploaded_file($temp_file_path, $target_file_path);
+//                if ($file_name === $main_image) {
+//                    $thumbnail_image_uri = $image_uri;
+//                }
+
+//                $temp_file_path = $_FILES['images']['tmp_name'][$key];
+//                $stored_dir = sprintf('%s%s', dirname(__DIR__) . "public/assets/images/", $uniqu_file_name);
+//                move_uploaded_file($temp_file_path, $stored_dir);
             }
         }
 
-        $post_client = new PostClient();
-        $payload = new IndexPostDto(user_id: 2, title: $title, body: $body, thumbnail_id: $thumbnail_image_id);
-//      post_id を返したい
-        $post_client->createPost($payload);
-//      imageClientを作ってimagesテーブルに投稿(post_id)と紐付けて画像を保存する
+//        $post_client = new PostClient();
+//        $payload = new IndexPostDto(user_id: 2, title: $title, body: $body, thumbnail_id: $thumbnail_image_uri);
+////      post_id を返したい
+//        $post_client->createPost($payload);
+        var_dump($image_uri_list);
+        $image_client = new ImageClient();
+        $image_client->createMultiImageForPost(post_id: 16, image_list: $image_uri_list);
 
         header("Location: http://localhost:8080", true, 303);
     }
