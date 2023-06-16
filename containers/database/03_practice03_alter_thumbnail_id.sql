9c392cdcc891d7c260da08dbb921f3d9752abb0e
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-06-17 00:06:23 +0900

    Add ImageRepository

---
+++ containers/database/03_practice03_alter_thumbnail_id.sql
@@ -0,0 +1,1 @@
+ALTER TABLE posts ALTER COLUMN thumbnail_id TYPE VARCHAR(10000);
---
+++ src/Repository/ImageRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repository;
+
+use App\Lib\Singleton\PgConnect;
+
+class ImageRepository
+{
+    public function __construct(
+        public ?\PDO $pdo = null)
+    {
+        if (is_null($pdo)) $this->pdo = PgConnect::getClient();
+    }
+
+    public function createMultiImageForPost(int $post_id, array $image_list)
+    {
+        $query = "INSERT INTO images (post_id, url) VALUES (?, ?)";
+        $stmt = $this->pdo->prepare($query);
+
+        foreach ($image_list as $image) {
+            $stmt->bindParam(1, $post_id);
+            $stmt->bindParam(2, $image);
+            $stmt->execute();
+        }
+    }
+
+}
